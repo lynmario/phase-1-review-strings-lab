15145660d1ca5e4987b2d9b10f5f13bded82633b
maryrine onsare <maryrine.onsare@student.moringaschool.com>
2022-06-09 02:46:43 +0300

    commit tree f47d0d9f31c1e74dd5d1823d9790f5dcb0383214

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // Write your code in this file!
+const currentUser='Maryrine Onsare';
+const welcomeMessage =`Welcome to Flatbook, $'{currentUser}'!`
+
+
+
+const excitedWelcomeMessage=welcomeMessage.toUpperCase();
+const shortGreeting=`Welcome, ${currentUser.slice(0,1)}!`
+  
 
 
 
 
- const currentUser = 'Grace Hopper';
 
- const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
- const excitedWelcomeMessage = ' WELCOME TO FLATBOOK, '  + 'GRACE HOPPER'+'!'
-const shortGreeting='Welcome, ' + 'Grace' + '!'
 
  
